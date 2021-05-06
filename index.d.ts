c688f9c7dae49c0bf7254a8e91e25c83274009d9
Socheat Sok <alex@socheat.net>
2021-05-06 12:51:51 +0700

    feat: ran dts-gen

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,15 @@
-export class Bus {
-    constructor()
-}
+// Type definitions for quick-bus 0.3
+// Project: https://github.com/baz/foo (Does not have to be to GitHub, but prefer linking to a source code repository rather than to a project website.)
+// Definitions by: Socheat Sok <https://github.com/me>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/*~ If this module is a UMD module that exposes a global variable 'myLib' when
+ *~ loaded outside a module loader environment, declare that global here.
+ *~ Otherwise, delete this declaration.
+ */
+export as namespace Bus;
+
+/*~ If this module has methods, declare them as functions like so.
+ */
+export function on(a: string): string;
+export function off(a: number): number;
